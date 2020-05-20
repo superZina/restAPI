c801f1f50ceeb15a8b5cae7cef9aee9629404052
이진하 <dkwlsfk2323@gmail.com>
2020-05-20 23:30:47 +0900

    get 방식을 이용해 서버로부터 json data 받아오기

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const bodyparser = require('body-parser')
 
 const server = express();
-
+server.use(bodyparser.json());
 const users = [
     {
         id:"dfdfdfdf",
@@ -17,8 +18,22 @@ const users = [
 
 server.get("/api/user",function(req,res){
     res.json(users);
+});
+server.get("/api/user/:id",(req,res) => { //:id ㄱㅏ id를 파라미터로 받는다는뜻
+    const user = users.find((u)=>{
+        return u.id === req.params.id;
+    });
+    if(user){
+        res.json(user);
+    }else{
+        res.status(404).json({errorMessage: "User not found"});
+    }
+});
+server.post("/api/user",function(req,res){
+    console.log(req.body);
+    users.push(req.body);
+    res.json(users);
 })
-
 server.listen(3000 , function(){
     console.log("Server is running");
 })
